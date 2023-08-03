9e3ef49cd54af6432a6d417b7d1674803630d39e
mhad00 <manhhad2002@gmail.com>
2023-08-04 01:00:47 +0700

    ?

---
+++ src/Page/Detail/ReadMore/Readmore.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const Readmore = ({ p, max }) => {
+  const [readMore, setReadMore] = useState(true);
+
+  const toggleReadMore = () => {
+    setReadMore(!readMore);
+  };
+
+  if (p.length <= max) {
+    return <p>{p}</p>;
+  }
+
+  return (
+    <div>
+      <h4>{readMore ? `${p.slice(0, max)}...` : p}</h4>
+      <h4 className="readMore" onClick={toggleReadMore}>{readMore ? `Show More` : `Show Less`}</h4>
+    </div>
+  );
+};
+
+export default Readmore;
